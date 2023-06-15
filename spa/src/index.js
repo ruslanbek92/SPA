4fae224c05291730244e079750f3c01111c83aa6
Ruslanbek <aksirus89@gmail.com>
2023-06-15 10:13:49 +0500

    feat: add additional global files

---
+++ spa/src/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import App from './app';
+// import AppSpa from './spa/app';
+
+const app = new App();
+
+// Для запуска приложения c реализацией SPA
+// const appSpa = new AppSpa();
